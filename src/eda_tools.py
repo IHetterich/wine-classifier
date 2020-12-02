002dd5652718fd659f7af956cbf47ef993a5e98b
Ian Hetterich <ihetterich@gmail.com>
2020-12-02 11:43:51 -0700

    EDA graphng implementation continueing

--- src/eda_tools.py
+++ src/eda_tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 def top_x_words(vectorizer, model, num):
     feature_words = vectorizer.get_feature_names()
@@ -11,3 +12,19 @@ def top_x_words(vectorizer, model, num):
         i_topn = np.argsort(log_prob)[::-1][:num]
         features_topn = [feature_words[i] for i in i_topn]
         print(f"Top {num} tokens: ", features_topn)
+
+def graph_top_num(data_handler, num):
+    varietals = list(data_handler.freq_dict.keys())
+    counts = list(data_handler.freq_dict.values())
+    sort_idx = np.argsort(counts)
+    top_idx = sort_idx[-1:-(num+1):-1]
+
+    top_varietals = [varietals[idx] for idx in top_idx]
+    top_counts = [counts[idx] for idx in top_idx]
+
+    fig, ax = plt.subplots(figsize=(15,15), dpi=100)
+    x = list(range(num))
+    ax.bar(x, top_counts, tick_label=top_varietals)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
--- src/work_space.py
+++ src/work_space.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 from data_handler import Data_Handler
 from sklearn.feature_extraction.text import TfidfVectorizer
-from eda_tools import top_x_words
+from eda_tools import top_x_words, graph_top_num
 from sklearn.naive_bayes import MultinomialNB, ComplementNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import spacy
 
 if __name__ == '__main__':
     # wrangler = Data_Handler('data/cleaned_data.csv')
@@ -18,18 +18,21 @@ if __name__ == '__main__':
     # model.fit(X, pn['variety'])
     # top_x_words(vecto,model, 10)
 
-    wrangler = Data_Handler('data/cleaned_data.csv')
-    stops = wrangler.stop_words
-    tfidf = TfidfVectorizer(stop_words=stops)
+    # wrangler = Data_Handler('data/cleaned_data.csv')
+    # stops = wrangler.stop_words
+    # tfidf = TfidfVectorizer(stop_words=stops)
 
-    df = wrangler.get_top_num(15)
-    y = df['variety']
-    X = df['description']
-    tfidf = TfidfVectorizer(stop_words=stops)
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
-    tfidf.fit(X_train)
-    X_train, X_test = tfidf.transform(X_train), tfidf.transform(X_test)
-    bayes = ComplementNB()
-    bayes.fit(X_train, y_train)
-    print(bayes.score(X_train, y_train))
-    print(bayes.score(X_test, y_test))
+    # df = wrangler.get_certain_varieties(['Chardonnay', 'Malbec'])
+    # y = df['variety']
+    # X = df['description']
+    # tfidf = TfidfVectorizer(stop_words=stops)
+    # X_train, X_test, y_train, y_test = train_test_split(X, y)
+    # tfidf.fit(X_train)
+    # X_train, X_test = tfidf.transform(X_train), tfidf.transform(X_test)
+    # bayes = ComplementNB()
+    # bayes.fit(X_train, y_train)
+    # print(bayes.score(X_train, y_train))
+    # print(bayes.score(X_test, y_test))
+
+    wrangler = Data_Handler('data/cleaned_data.csv')
+    graph_top_num(wrangler, 15)
